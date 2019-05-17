cc838a4fc9c67d5927750d7c72ffb96405a36d8c
chandni <chandnibpatel@gmail.com>
2019-05-16 23:23:15 -0500

    changes to saved page for rendering issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import Nav from "./components/Nav";
 import Search from "./pages/Search";
+import Saved from "./pages/Saved";
+import { Route, Redirect, Switch, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
+    <Router>
     <div>
-      <Nav/>
-      <Search />
+        <Nav />
+        <Switch>
+            <Route path="/search" component={Search} />
+            <Route path="/saved" component={Saved} />
+            <Redirect from="/" exact to="/search" />
+        </Switch>
     </div>
+    </Router>
   );
 }
 
